bcd477149a21a10651efb5c26a929454b16b5cae
Lynn-Becky <60915579+Lynn-Becky@users.noreply.github.com>
2024-01-03 21:34:33 +0800

    Update v4.sh
    增加提示，新增一键关闭Nat64功能

--- v4.sh
+++ v4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "一键在仅限ipv6主机上访问ipv4脚本 @lynn_becky"
+echo "一键在仅限ipv6主机上访问ipv4脚本 github@Lynn_Becky"
 echo "NAT64在执行github脚本时（如安装探针）可不添加proxychains选项，但速度可能较慢"
 echo "Warp往往有着更快的速度，更干净的ip，但在部分主机上并非是全局的，需要进行配置"
 echo "个人建议先使用NAT64进行开局，安装所需要的脚本，再使用warp做为日常使用"
@@ -18,7 +18,7 @@ case $choice in
     echo "原文件已经备份，恢复请使用选项3'"
     ;;
   2)
-    echo "你选择了Warp服务"
+    echo "你选择了Warp服务，默认socs端口为1835"
     apt update && apt install -y curl gnupg lsb-release proxychains4
     curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg |  gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
