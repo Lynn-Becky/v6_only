f2f75faf2edb19f1b8f5f8a7a55ce29ce010bceb
Lynn-Becky <60915579+Lynn-Becky@users.noreply.github.com>
2024-01-09 08:24:29 +0800

    Update v4.sh
    1.增加脚本使用时的可读性
    2.增加持久化NAT64服务的相关选项
    3.修改部分描述

--- v4.sh
+++ v4.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-echo "一键在仅限ipv6主机上访问ipv4脚本 github@Lynn_Becky"
-echo "NAT64在执行github脚本时（如安装探针）可不添加proxychains选项，但速度可能较慢"
-echo "Warp往往有着更快的速度，更干净的ip，但在部分主机上并非是全局的，需要进行配置"
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' 
+
+echo -e "${GREEN}一键在仅ipv6系统上添加ipv4出口脚本 @lynn-becky${NC}"
+echo "NAT64是全局的，在执行github脚本时（如安装探针）可不添加proxychains选项，但速度可能较慢"
+echo "Warp往往有着更快的速度，但并非是全局的，需要进行配置才可使用他"
 echo "个人建议先使用NAT64进行开局，安装所需要的脚本，再使用warp做为日常使用"
 echo "二者请只使用其中之一"
 echo "1. 使用NAT64服务"
 echo "2. 使用Warp服务"
-echo "3. 恢复为正常DNS"
+echo "3. 不再使用NAT64服务"
+echo "4. 持久化NAT64"
+echo "5. 不再持久化NAT64"
 read -p "请输入选项数字: " choice
 
 case $choice in
   1)
-    echo "你选择了NAT64服务"
+    echo -e "${YELLOW}你选择了NAT64服务${NC}"
     cp /etc/resolv.conf /etc/resolv.conf.bak
     echo -e "nameserver 2a01:4f8:c2c:123f::1\nnameserver 2a00:1098:2c::1\nnameserver 2a01:4f9:c010:3f02::1" > /etc/resolv.conf
-    echo "原文件已经备份，恢复请使用选项3'"
+    echo -e "${GREEN}在lxc虚拟化的系统上，重启会导致NAT64服务失效，需使用对应命令修复${NC}"    
+    echo -e "${GREEN}执行完毕${NC}"
     ;;
   2)
-    echo "你选择了Warp服务，默认socks端口为1835"
+    echo -e "${YELLOW}你选择了Warp服务${NC}"
     apt update && apt install -y curl gnupg lsb-release proxychains4
     curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg |  gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
@@ -31,17 +39,29 @@ case $choice in
     apt install wget
     curl -Ls https://raw.githubusercontent.com/Lynn-Becky/v6_only/main/proxychains4.conf -o proxychains4.conf
     mv proxychains4.conf /etc/proxychains4.conf 
+    echo -e "${GREEN}执行完毕${NC}"
     ;;
   3)
-    echo "你选择了恢复为正常DNS"
+    echo -e "${YELLOW}你选择了恢复为正常DNS${NC}"
     if [ -e /etc/resolv.conf.bak ]; then
       cp /etc/resolv.conf.bak /etc/resolv.conf
-      echo "DNS配置已恢复为正常状态"
+      echo -e "${GREEN}DNS配置已恢复为正常状态${NC}"
     else
-      echo "备份文件不存在，无法恢复DNS配置"
+      echo -e "${RED}备份文件不存在，无法恢复DNS配置${NC}"
     fi
     ;;
+  4)
+    echo -e "${YELLOW}你选择了持久化NAT64${NC}"
+    touch /etc/.pve-ignore.resolv.conf
+    echo -e "${GREEN}执行完毕${NC}"
+    ;;
+  5)
+    echo -e "${YELLOW}你选择了不再持久化NAT64${NC}"
+      rm /etc/.pve-ignore.resolv.conf
+      echo -e "${GREEN}执行完毕${NC}"
+    ;;
   *)
-    echo "无效的选项，请重新运行脚本并选择有效的选项。"
+    echo -e "${RED}无效的选项，请重新运行脚本并选择有效的选项。${NC}"
+    exit 1
     ;;
 esac
